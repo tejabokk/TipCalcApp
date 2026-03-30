0e3294ba875aa2bfaf72732e77be0f4206f27d68
Tejaswi Bokkisam <venkatasaitejaswi@gmail.com>
2026-03-30 00:40:54 -0500

    Initial Commit

---
+++ TipCalcApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipCalcApp
+//
+//  Created by Tejaswi Bokkisam on 3/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipCalcApp/TipCalcAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcAppApp.swift
+//  TipCalcApp
+//
+//  Created by Tejaswi Bokkisam on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
